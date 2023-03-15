d85b8be1d89be9929dd93c7c1b03c87de2359708
MinhHieuVux <minhhieuvux@gmail.com>
2023-03-15 14:01:56 +0700

    update

--- src/main/java/net/codejava/config/WebSecurityConfig.java
+++ src/main/java/net/codejava/config/WebSecurityConfig.java
@@ -52,7 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.formLogin()
 				.usernameParameter("email")
-				.defaultSuccessUrl("/users")
+				.defaultSuccessUrl("/")
 				.permitAll()
 			.and()
 			.logout().logoutSuccessUrl("/").permitAll();
